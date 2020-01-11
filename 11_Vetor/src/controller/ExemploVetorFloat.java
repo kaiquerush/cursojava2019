29e446308a3d97636b1f6ae66498ec081197307b
Aluno <Aluno@ALU22202.educa.sf>
2020-01-10 21:43:42 -0300

    Vetores senhorita vetores vetores

---
+++ 11_Vetor/src/controller/ExemploVetorFloat.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class ExemploVetorFloat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float notas [] = {4, 7.5f, 10, 9 };
+System.out.println("notas[1] = " + notas [1] );
+
+	}
+
+}
